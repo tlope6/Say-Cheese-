edc8fe7cc124e57dbdd1f6b441819ee7a4acd3ac
tlope6 <tatianamlopez27@gmail.com>
2026-01-11 01:01:26 -0600

    Adding more to the project

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import sys
 from PyQt5.QtWidgets import QApplication
-from ui import AppUI
-from camera import capture_frame
-from database import log_action
+from ui.ui import AppUI
+from vision.camera import capture_frame
+from data.database import log_action
 
 
 def handle_click() :
